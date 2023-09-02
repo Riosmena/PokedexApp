47ad0ecc50c95cf45f84749161664a467c1b3680
Emi Riosmena <emi.riosmena@gmail.com>
2023-09-01 18:53:30 -0600

    Lab3 Data Classes y RecyclerView

---
+++ PokedexApp/app/src/main/java/com/example/kotlin/mypokedexapp/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.kotlin.mypokedexapp
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.kotlin.mypokedexapp.databinding.ActivityMainBinding
+
+class MainActivity: Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter : PokemonAdapter = PokemonAdapter()
+    private lateinit var data:ArrayList<PokemonBase>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        initializeBinding()
+        setUpRecyclerView(testData())
+    }
+
+    private fun initializeBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun testData():ArrayList<PokemonBase>{
+        var result = ArrayList<PokemonBase>()
+
+        result.add(PokemonBase("bulbasaur",""))
+        result.add(PokemonBase("charmander",""))
+        result.add(PokemonBase("squirtle",""))
+        result.add(PokemonBase("ninetales",""))
+        result.add(PokemonBase("lucario",""))
+        result.add(PokemonBase("houndoom",""))
+        result.add(PokemonBase("pikachu",""))
+        result.add(PokemonBase("vaporeon",""))
+        result.add(PokemonBase("magikarp",""))
+        result.add(PokemonBase("charizard",""))
+
+        return result
+    }
+
+    private fun setUpRecyclerView(dataForList:ArrayList<PokemonBase>){
+        binding.RVPokemon.setHasFixedSize(true)
+        val linearLayoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false)
+        binding.RVPokemon.layoutManager = linearLayoutManager
+        adapter.PokemonAdapter(dataForList)
+        binding.RVPokemon.adapter = adapter
+    }
+
+}
