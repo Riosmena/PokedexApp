31eaa1f2a6d4dee66452b10fe50082314776097a
Riosmena <emi.riosmena@gmail.com>
2023-09-18 19:50:44 -0600

    Progress on Lab6

--- PokedexApp/app/src/main/java/com/example/kotlin/mypokedexapp/framework/views/fragments/PokedexFragment.kt
+++ PokedexApp/app/src/main/java/com/example/kotlin/mypokedexapp/framework/views/fragments/PokedexFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin.mypokedexapp.R
@@ -62,11 +63,13 @@ class PokedexFragment: Fragment() {
 
     private fun setUpRecyclerView(dataForList:ArrayList<PokemonBase>){
         recyclerView.setHasFixedSize(true)
-        val linearLayoutManager = LinearLayoutManager(
+        /*val linearLayoutManager = LinearLayoutManager(
+            requireContext(),        LinearLayoutManager.VERTICAL,        false)*/    val gridLayoutManager = GridLayoutManager(
             requireContext(),
-            LinearLayoutManager.VERTICAL,
-            false)
-        recyclerView.layoutManager = linearLayoutManager
+            3,
+            GridLayoutManager.VERTICAL,
+            false    )
+        recyclerView.layoutManager = gridLayoutManager
         adapter.PokemonAdapter(dataForList,requireContext())
         recyclerView.adapter = adapter
     }
